6cb4c4a73001bbb02a62d62cca8aeaa93b8dceb9
cornboy <cornboy@tutanota.com>
2016-11-14 15:07:08 -0500

    Correct SORT_BY in scraper

--- js/scrappers/movies/yts.js
+++ js/scrappers/movies/yts.js
@@ -2,7 +2,7 @@ fetcher.scrappers.yts = function(genre, keywords, page, callback){
 
 
 
-		var url = 'http://yts.to/api/list.json?genre=' + genre + '&sort=seeds&limit=50&set=' + ui.home.catalog.page;
+		var url = 'http://yts.to/api/list.json?genre=' + genre + '&sort_by=seeds&limit=50&set=' + ui.home.catalog.page;
 
         if (keywords) {
             url += '&keywords=' + keywords;
